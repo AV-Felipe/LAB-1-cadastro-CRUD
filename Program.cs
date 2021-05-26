61f4e3904e08e5962d825e7b6f56e7a500b29810
AV-Felipe <aerogrado@gmail.com>
2021-05-26 16:12:09 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LAB_1_cadastro_CRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = menuInicial();
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+              switch (switch_on)
+              {
+                case "1":
+                    //ListarSeries();
+                    break;
+                case "2":
+                    //InserirSerie();
+                    break;
+                case "3":
+                    //AtualizarSerie();
+                    break;
+                case "4":
+                    //ExcluirSerie();
+                    break;
+                case "5":
+                    //VisualizarSerie();
+                    break;
+                case "C":
+                    Console.Clear();
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException();
+              }
+              opcaoUsuario = menuInicial();
+            }
+       
+            Console.WriteLine("Hasta la vista, baby!");
+            Console.WriteLine();
+        }
+    
+
+        // STATIC CLASSES
+
+        private static string menuInicial()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Bem-vindo ao nosso pequeno acervo de séries e outras coisas supimpas!");
+            Console.WriteLine("Informe a opção desejada:");
+            Console.WriteLine();
+            Console.WriteLine("1- Listar séries");
+            Console.WriteLine("2- Inserir nova série");
+            Console.WriteLine("3- Atualizar série");
+            Console.WriteLine("4- Excluir série");
+            Console.WriteLine("5- Visualizar série");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,19 @@
+# Digital Innovation One - Prática .NET
+
+## Bootcamp Take Blip
+
+### Projeto Prático 1
+
+Aplictivo CRUD, em linha de comando, para o cadastro de séries (ou filmes, ou livros, etc.) em memória.
+
+Utilizaremos uma classe abstrata, contendo as informações gerais dos itens que trataremos nos cadastros, a saber:
+ - ID
+ - Título
+ - Ano
+ 
+Da qual derivaremos as classes para cada classe de item do cadastro, cada uma com suas particularidades, por exemplo:
+ - Séries precisará de campos para temporada e episódio
+ - Livros precisará de campos para edição
+ 
+Utilizaremos uma interface, para padronizar as operações básicas do CRUD no nosso repositório (uma lista de elementos).
+ 
